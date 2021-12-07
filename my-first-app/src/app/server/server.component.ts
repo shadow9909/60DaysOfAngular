89c1e90cfeeb309d0d94733ea2a321df3576b9ae
Archit Maheshwari <arcmahes@cisco.com>
2021-12-07 23:39:33 +0530

    creating new components

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  // template: '<div> server Component</div>',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {}
