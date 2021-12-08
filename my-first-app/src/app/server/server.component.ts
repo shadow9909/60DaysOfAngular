e3b6812a2fe5037c46570016e6f7d5170bb172f4
Archit Maheshwari <arcmahes@cisco.com>
2021-12-08 22:27:49 +0530

    ngClass directive

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  // template: '<div> server Component</div>',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css'],
+  styles: ['.online { color: white; }'],
 })
 export class ServerComponent {
   serverId: number = 10;
