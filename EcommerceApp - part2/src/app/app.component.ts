c7f4feea092149ef5a5357a10282dbfa59a07648
Archit Maheshwari <arcmahes@cisco.com>
2021-12-09 23:19:02 +0530

    ecom-2->using json-server data

---
+++ EcommerceApp - part2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app",
+  template: "<store></store>",
+})
+export class AppComponent {}
+
+//<router-outlet> </router-outlet> its a placeholder component,
+//it serves the component whichever the route provides
+
+// routing mechanism :
---
+++ EcommerceApp - part2/src/app/store/cartSummary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
+
+@Component({
+  selector: "cart-summary",
+  template: "<div>total items & price</div>",
+})
+export class CartSummaryComponent {
+  // constructor(public cart: Cart) { }
+}
