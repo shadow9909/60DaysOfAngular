84e564cec71cee415bfcd93b3becc0bd5c0d1dd1
Archit Maheshwari <arcmahes@cisco.com>
2021-12-11 22:32:01 +0530

    shopping list/edit added

--- RecipeApp/RecipeApp/src/app/shopping-list/shopping-list.component.ts
+++ RecipeApp/RecipeApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('apple', 10),
+    new Ingredient('pie', 5),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
